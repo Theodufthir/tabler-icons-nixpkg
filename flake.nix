130fdce1342dd81a58a623ae0bfeb4ba11746745
Théo Dufour <theo.dufthir@gmail.com>
2024-05-19 15:19:51 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,57 @@
+{
+  description = "Tabler icons font flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      targetSystems = [ "x86_64-linux" "aarch64-linux" ];
+      buildFor = system: import nixpkgs { inherit system; };
+    in {
+      overlays.default = final: prev: {
+        inherit (self.packages.${prev.system}) tabler-icons;
+      };
+      
+      packages = nixpkgs.lib.genAttrs targetSystems (system:
+        let
+          pkgs = buildFor system;
+        in rec {
+          tabler-icons = pkgs.callPackage ({ lib, fetchzip, stdenvNoCC }:
+            stdenvNoCC.mkDerivation rec {
+              pname = "tabler-icons";
+              version = "3.4.0";
+
+              src = fetchzip {
+                url = "https://github.com/tabler/tabler-icons/releases/download/v${version}/tabler-icons-${version}.zip";
+                sha256 = "sha256-1utVHJKpdfR5N8bPFv5gi1npPCM63s0ak4LLZ+ATQKE=";
+                stripRoot = false;
+              };
+
+              installPhase = ''
+                runHook preInstall
+
+                mkdir -p "$out/share/fonts/"{ttf,woff,woff2}
+                cp webfont/fonts/tabler-icons.ttf "$out/share/fonts/ttf/"
+                cp webfont/fonts/tabler-icons.woff "$out/share/fonts/woff/"
+                cp webfont/fonts/tabler-icons.woff2 "$out/share/fonts/woff2/"
+
+                runHook postInstall
+              '';
+
+              meta = with lib; {
+                description = "Tabler icons from in its webfont format";
+                longDescription = '''';
+                homepage = "https://tabler.io/icons";
+                license = licenses.mit;
+                platforms = platforms.all;
+                maintainer = [];
+              };
+            }) {};
+
+          default = tabler-icons;
+        }
+      );
+    };
+}
